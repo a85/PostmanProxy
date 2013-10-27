7d2c805a92941b58f8010fe744c4f842cbedf1aa
Abhinav Asthana <rickyast@gmail.com>
2013-10-27 16:03:54 +0530

    Added options for naming and method filtering

--- postmanproxy/main.py
+++ postmanproxy/main.py
@@ -13,13 +13,27 @@ def signal_handler(signal, frame):
 	print collection.save()
 	sys.exit(0)
 
-def main():
+def start_creator_proxy(options):
 	global collection
+
+	name = options.name
+	path = options.path
+	host = options.host
+	methods = options.methods
+	status_codes = options.status_codes
+
 	print "Hey! This is the Postman proxy!"
 	print "Press Ctrl+C to stop the proxy"
-	rules = []
 
-	collection = Collection("Postman")
+	rules = {
+		'host': host,
+		'methods': methods,
+		'status_codes': status_codes
+	}
+
+	print "Rules are", rules
+
+	collection = Collection(name)
 	config = proxy.ProxyConfig()
 	server = proxy.ProxyServer(config, 8080)
 	m = CollectionCreatorProxy(server, collection, rules)
@@ -29,6 +43,29 @@ def main():
 	signal.signal(signal.SIGINT, signal_handler)
 	print 'Press Ctrl+C again to save the collection'
 	signal.pause()
+
+def start_filter_proxy(options):
+	pass
+
+def main():
+	parser = OptionParser(usage="Usage: %prog [options] filename")
+	parser.add_option("-o", "--operation", dest="operation", help="filter/save. Default is save", default="save")
+	parser.add_option("-n", "--name", dest="name", help="Collection name", default="default")
+	parser.add_option("-p", "--path", dest="path", help="Target path for saving the collection", default="")
+	parser.add_option("-t", "--host", dest="host", help="Only allow URLs of this host", default="")
+	parser.add_option("-m", "--methods", dest="methods", help="Comma separated list of allowed methods. Default is all methods", default=[])
+	parser.add_option("-s", "--status_codes", dest="status_codes", help="Comma separated list of allowed status codes. Default is all codes", default=[])
+
+	(options, args) = parser.parse_args()
+
+	if options.operation == "save":
+		start_creator_proxy(options)
+	elif options.operation == "filter":
+		start_filter_proxy(options)
+	else:
+		start_creator_proxy(options)
+
+
 
 if __name__ == "__main__":
     main()
--- postmanproxy/postman/collection_creator_proxy.py
+++ postmanproxy/postman/collection_creator_proxy.py
@@ -7,7 +7,32 @@ class CollectionCreatorProxy(controller.Master):
 	def __init__(self, server, collection, rules):
 		self.collection = collection
 		self.rules = rules
+		self.host = rules['host']
+		self.methods = self.get_methods(rules['methods'])
+		self.status_codes = self.get_status_codes(rules['status_codes'])
+
+		print self.host, self.methods, self.status_codes
+
 		controller.Master.__init__(self, server)
+
+	def get_methods(self, methodString):
+		m = methodString.split(',')
+		methods = []
+		for method in m:
+			method = method.strip()
+			method = method.upper()
+			methods.append(method)
+
+		return methods
+
+	def get_status_codes(self, statusCodeString):
+		c = statusCodeString.split(',')
+		status_codes = []
+		for status_code in c:
+			status_code = status_code.strip()
+			status_codes.append(int(status_code))
+
+		return status_codes
 
 	def run(self):
 		try:
@@ -19,7 +44,24 @@ class CollectionCreatorProxy(controller.Master):
 		request = Request(self.collection.id)
 		request.init_from_proxy(msg)
 
-		self.collection.add_request(request)
+		allowed_host = True
+		allowed_method = True
+		allowed_status_code = True
+
+		if self.host != "":
+			if self.host == msg.host:
+				allowed_host = True
+			else:
+				allowed_host = False
+
+		if len(self.methods) > 0:
+			if msg.method in self.methods:
+				allowed_method = True
+			else:
+				allowed_method = False
+
+		if allowed_method and allowed_host and allowed_status_code:
+			self.collection.add_request(request)
 
 		msg.reply()
 
